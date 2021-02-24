8f9ed6cefcc432d746207a8f028d380462e73761
datokey <datokey11@gmail.com>
2021-02-24 10:59:37 +0700

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
-    return ( 
-        <div className="flex justify-center px-4 py-1 border-b border-gray-600 shadow-2xl">
-            <div className="grid grid-cols-12 border-2 border-red-300 space-x-3">
-                <Link to="/" className="col-span-6 border-red-300 border-r-2 p-2">Home </Link>
-                <Link to="/about" className="col-span-2 border-red-300 border-r-2 p-2">About</Link>
-                <Link to="/create" className="col-span-2 border-red-300 border-r-2 p-2">Create</Link>
-                <Link to="/detail" className="col-span-2 border-red-300 p-2">link ke detail</Link>
-            </div>
-            <div className="px-32 bg-red-800">
-                <div>search</div>
 
-            </div>
+const Navbar = () => {
+    return (
+      <div className="flex justify-center px-4 py-1 border-b border-gray-600 shadow-2xl">
+        <div className="p-3 space-x-3 flex">
+          <Link to="/" className="border-r-2 border-gray-700 pr-3">
+            <h3 className="hover:bg-gray-700 hover:text-gray-200 rounded-xl p-1 ">
+              Home
+            </h3>
+          </Link>
+          <Link to="/about" className="border-r-2 border-gray-700 pr-3 ">
+            <h3 className="hover:bg-gray-700 hover:text-gray-200 rounded-xl p-1">
+              About
+            </h3>
+          </Link>
+          <Link to="/create" className=" pr-3 ">
+            <h3 className="hover:bg-gray-700 hover:text-gray-200 rounded-xl p-1 ">
+              Create
+            </h3>
+          </Link>
         </div>
-     );
+      </div>
+    );
 }
  
 export default Navbar;
